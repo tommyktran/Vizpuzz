2487ce9f8c5dbd60b4426038a3432bf311a07c5a
Tommy Tran <tommyktran@gmail.com>
2021-02-02 20:01:01 -0500

    Add API

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const chess = new Chess();
 
 let movesToVisualizeEl = document.getElementById("movesToVisualize");
 
-function displayGameMove(pgn, moveNumber, player = white) {
+function displayGameMove(pgn, moveNumber, player = "white") {
     chess.load_pgn(pgn, {sloppy: true})
     let moves = chess.history();
     chess.reset();
@@ -40,7 +40,7 @@ function updateMovestoVisualize(string) {
     movesToVisualizeEl.innerHTML = string;
 }
 function capture() {
-    document.getElementById("capture").style="background-color: hsl(218 7% 17% / 1);"
+    // document.getElementById("capture").style="background-color: hsl(218 7% 17% / 1);"
 
     html2canvas(document.querySelector("#capture")).then(canvas => {
         document.body.appendChild(canvas)
@@ -63,18 +63,141 @@ function hideModal() {
 
 }
 function submit() {
+
     let level = document.getElementById("level").value
+    let gameId = document.getElementById("gameId").value
+    let moveNumber = document.getElementById("moveNumber").value
+    console.log("AAAAA"+moveNumber)
+    if (gameId && moveNumber) { 
+        lichessTactictoPuzzle(gameId, moveNumber, level)
+    }
+
     let rating = document.getElementById("rating").value
     let fen = document.getElementById("fen").value
     let moves = document.getElementById("moves").value
 
+    if (level == 1) {
+        level = "One";
+    } else if (level == 2) {
+        level = "Two";
+    } else if (level == 3) {
+        level = "Three";
+    } else if (level == 4) {
+        level = "Four";
+    } else if (level == 5) {
+        level = "Five";
+    } else if (level == 6) {
+        level = "Six";
+    }
+
     document.getElementById("website-subtitle").innerHTML = "Level " + level
+
+
     document.getElementById("puzzleRating").innerHTML = "Rating: " + rating
     displayGameFEN(fen)
     document.getElementById("movesToVisualize").innerHTML = moves
 
     hideModal()
 }
+
+function makePuzzle(string) {
+    string = string.split("/")
+    if (string.length == 3 || string.length == 4) {
+        if (string.length == 4) {
+           document.getElementById("level").value = string[3]
+        }
+        
+        document.getElementById("rating").value = ""
+        document.getElementById("gameId").value = string[2]
+        document.getElementById("moveNumber").value = string[3]
+        document.getElementById("fen").value = string[4]
+        document.getElementById("moves").value = string[5]
+    }
+    document.getElementById("level").value = string[0]
+    document.getElementById("rating").value = string[1]
+    document.getElementById("gameId").value = string[2]
+    document.getElementById("moveNumber").value = string[3]
+    document.getElementById("fen").value = string[4]
+    document.getElementById("moves").value = string[5]
+    submit()
+}
+
+function lichessTactictoPuzzle(gameId, moveNumber, movesBack) {
+    let player = "white";
+    console.log(moveNumber)
+    toString(moveNumber)
+    
+    // moveNumber should be the number if white is to move or the number + ... if black is to move
+    if (!(moveNumber.includes("..."))) {
+        moveNumber.replace("...", "")
+        parseInt(moveNumber)
+        moveNumber = moveNumber * 2 - 2
+        player = "black"
+    } else {
+        console.log(moveNumber)
+        moveNumber = moveNumber.replace("...", "")
+        console.log(moveNumber)
+
+        parseInt(moveNumber)
+        console.log(moveNumber)
+
+        moveNumber = moveNumber * 2 - 1
+        console.log(moveNumber)
+
+        player = "white"
+    }
+
+    console.log(moveNumber)
+    movesBack = movesBack * 2
+    moveNumber -= movesBack
+
+
+    let movesToVisualize = []
+
+    console.log(moveNumber)
+    console.log(movesBack)
+
+
+    getLichessGamebyId(gameId).then(data => data.text())
+        .then(result => {
+            console.log(result)
+            chess.load_pgn(result, {sloppy: true})
+            let moves = chess.history();
+            console.log(moves)
+            chess.reset();
+            for (let y = 0; y <= moveNumber; y++) {
+                chess.move(moves[y]);
+            }
+            board1 = Chessboard('board1', {orientation: player})
+            board1.position(chess.fen());
+            
+            for (let y = 1; y <= movesBack; y++) {
+                movesToVisualize.push(moves[moveNumber + y])
+                console.log(movesToVisualize)
+            }
+
+            document.getElementById("movesToVisualize").innerHTML = movesToVisualize.join(" ")
+        })
+}
+
+async function getLichessGamebyId(id) {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      return fetch("https://lichess.org/game/export/" + id + "?clocks=false&evals=false", requestOptions)
+        // .then(response => response.text())
+        // .then(result => {console.log(result);})
+        .catch(error => console.log('error', error));
+}
+
+
+
+lichessTactictoPuzzle("r97LGR27", "10", 2)
+
+// getLichessGamebyId("r97LGR27").then(data => data.text())
+//     .then(result => console.log(result))
 
 
 // async function getLichessGamebyId(id) {
